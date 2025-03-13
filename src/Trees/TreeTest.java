84e6d00f95f2232e88d5f5dabb6f08c170321c1f
Rakshit-15 <50164701+Rakshit-15@users.noreply.github.com>
2025-03-13 18:12:22 +0530

    adding new code which sums all node values

--- src/Trees/TreeTest.java
+++ src/Trees/TreeTest.java
@@ -2,6 +2,21 @@ package Trees;
 
 public class TreeTest {
 
+    public static int sumOfAllNode(TreeNode<Integer> root){
+
+        if(root==null) return 0;  // if there is no root then there will be sum as 0.
+
+        int sum=0;
+        // through this loop we will have the ans of all nodes through recursion and
+        //once we have that we will add root data to return the final ans.
+        for(int i=0;i<root.children.size();i++){
+            int ans=sumOfAllNode(root.children.get(i));
+            sum+=ans;
+        }
+        return sum+root.data;
+
+    }
+
     public static int countNodes(TreeNode<Integer> root){
 
          if(root==null) return 0;
@@ -34,26 +49,4 @@ public class TreeTest {
     }
 
 
-    public static void main(String[] args) {
-        TreeNode<Integer> root=new TreeNode<>(4);
-        TreeNode<Integer> node1=new TreeNode<>(2);
-        TreeNode<Integer> node2=new TreeNode<>(3);
-        TreeNode<Integer> node3=new TreeNode<>(1);
-        TreeNode<Integer> node4=new TreeNode<>(6);
-        TreeNode<Integer> node5=new TreeNode<>(5);
-
-        root.children.add(node1);
-        root.children.add(node2);
-        root.children.add(node3);
-
-        node2.children.add(node4);
-        node2.children.add(node5);
-
-//        printTree(root);
-        System.out.println(countNodes(root));
-
-
-
-
-    }
 }
